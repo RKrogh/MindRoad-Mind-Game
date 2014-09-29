74daa3b17b7816186a234f534980d2dd466d1a7f
RKrogh <krogh.robert@gmail.com>
2014-09-29 13:19:44 +0200

    Tog bort /script/

--- script/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-javac src/MindRoad/*.java
-javac -cp src/:testng-6.8/testng-6.8.jar Test/*.java
--- script/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp testng-6.8/testng-6.8.jar:Test:src org.testng.TestNG -verbose 2 Test/test.xml

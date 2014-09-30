42e4f0b46d3ca4f81d7b1366bf5dbd86cf46831a
RobKr <krogh.robert@gmail.com>
2014-09-30 11:21:28 +0200

    Added a statement to the persormance test.

--- test/PerformanceTest.java
+++ test/PerformanceTest.java
@@ -1,3 +1,4 @@
+import MindRoad.StartGame;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.lang.management.ManagementFactory;
@@ -5,15 +6,17 @@ import java.lang.management.ThreadMXBean;
 import static org.testng.AssertJUnit.*;
 
 public class PerformanceTest {
+    private StartGame game;
 
     @BeforeMethod
     public void setUp(){
+        game = new StartGame();
     }
 
     @Test
     public void testExecutionTime(){
         long startTime = ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
-        main.start();
+        game.start();
         long finnishTime = ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
         System.out.println(startTime);
         System.out.println(finnishTime);

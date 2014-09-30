b304f7f9c86baa1aa4bfcfa4031fc12543b465fb
RKrogh <krogh.robert@gmail.com>
2014-09-30 11:24:36 +0200

    Fix for performance testing.

--- test/PerformanceTest.java
+++ test/PerformanceTest.java
@@ -5,18 +5,20 @@ import java.lang.management.ThreadMXBean;
 import static org.testng.AssertJUnit.*;
 
 public class PerformanceTest {
+    private StartGame game;
 
     @BeforeMethod
     public void setUp(){
+        game = new StartGame();
     }
 
     @Test
     public void testExecutionTime(){
         long startTime = ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
-        main.start();
+        game.start();
         long finnishTime = ManagementFactory.getThreadMXBean().getCurrentThreadCpuTime();
         System.out.println(startTime);
         System.out.println(finnishTime);
-        assert (finnishTime-startTime<0.0000005);
+        assert (finnishTime-startTime<0.000005);
     }
 }

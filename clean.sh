65a3d96ce00f292ad1e335358f323f4d9a5aeb3a
Johan Bergqvist <bq@johanbq.se>
2014-10-07 21:39:02 +0200

    Added some output to clean.sh. Created test-coverage.sh.

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find . -name '*.class' | xargs rm
+rm -rf instrumented cobertura.ser cobertura_report test-output
---
+++ test-coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+COBERTURA=cobertura-2.0.3
+INSTRUMENTED=instrumented
+REPORTDIR=cobertura_report
+
+mkdir -f $INSTRUMENTED
+mkdir -f $REPORTDIR
+
+# Instrument the classes that we want to check coverage on
+$COBERTURA/cobertura-instrument.sh src/MindRoad/*.class --destination $INSTRUMENTED || exit
+
+# Run the tests
+java -cp $COBERTURA/$COBERTURA.jar:$INSTRUMENTED:testng-6.8/testng-6.8.jar:test org.testng.TestNG -verbose 2 test/test.xml
+
+# Generate report
+$COBERTURA/cobertura-report.sh --destination $REPORTDIR src
+
+# Check coverage
+$COBERTURA/cobertura-check.sh

9ffbc6cfb8f351b86b1514ed34ad1420d2bef226
Johan Bergqvist <bq@johanbq.se>
2014-10-07 21:38:02 +0200

    Fixed cobertura's incorrect classpaths.

--- cobertura-2.0.3/cobertura-instrument.sh
+++ cobertura-2.0.3/cobertura-instrument.sh
@@ -1,1 +1,4 @@
-java -cp `dirname $0`/cobertura.jar:`dirname $0`/lib/asm-3.3.1.jar:`dirname $0`/lib/asm-tree-3.3.1.jar:`dirname $0`/lib/asm-commons-3.3.1.jar:`dirname $0`/lib/log4j-1.2.9.jar:`dirname $0`/lib/jakarta-oro-2.0.8.jar net.sourceforge.cobertura.instrument.Main $*
+DIR=$(dirname $0)
+CP=$(find $DIR/cobertura-?.?.?.jar $DIR/lib -type f | tr '\n' :)
+
+java -cp $CP net.sourceforge.cobertura.instrument.Main $*

e7063a8bff28380cb61a4c317d360e8f181bd2d9
RKrogh <krogh.robert@gmail.com>
2014-10-10 10:41:00 +0200

    Instrumented test classes

--- test-coverage.sh
+++ test-coverage.sh
@@ -8,7 +8,7 @@ mkdir -p $INSTRUMENTED
 mkdir -p $REPORTDIR
 
 # Instrument the classes that we want to check coverage on
-$COBERTURA/cobertura-instrument.sh src/MindRoad/*.class --destination $INSTRUMENTED || exit
+$COBERTURA/cobertura-instrument.sh src/MindRoad/*.class test/*.class --destination $INSTRUMENTED || exit
 
 # Run the tests
 java -cp $COBERTURA/$COBERTURA.jar:$INSTRUMENTED:testng-6.8/testng-6.8.jar:test org.testng.TestNG -verbose 2 test/test.xml

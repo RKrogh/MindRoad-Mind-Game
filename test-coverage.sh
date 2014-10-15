0fc19f410bc90d9cfdb2a9f897e133300a3cc500
RKrogh <krogh.robert@gmail.com>
2014-10-15 17:13:17 +0200

    Added traces to test-coverage

--- test-coverage.sh
+++ test-coverage.sh
@@ -4,17 +4,20 @@ COBERTURA=cobertura-2.0.3
 INSTRUMENTED=instrumented
 REPORTDIR=cobertura_report
 
+rm -rf $INSTRUMENTED
+rm -rf $REPORTDIR
+rm -rf cobertura.ser
 mkdir -p $INSTRUMENTED
 mkdir -p $REPORTDIR
 
-# Instrument the classes that we want to check coverage on
+echo Instrument the classes that we want to check coverage on
 $COBERTURA/cobertura-instrument.sh src/MindRoad/*.class --destination $INSTRUMENTED || exit
 
-# Run the tests
+echo Run the tests
 java -cp $COBERTURA/$COBERTURA.jar:$INSTRUMENTED:testng-6.8/testng-6.8.jar:test org.testng.TestNG -verbose 2 test/test.xml
 
-# Generate report
+echo Generate report
 $COBERTURA/cobertura-report.sh --format xml --destination $REPORTDIR src
 
-# Check coverage
-#$COBERTURA/cobertura-check.sh
+echo Check coverage
+$COBERTURA/cobertura-check.sh --branch 0
